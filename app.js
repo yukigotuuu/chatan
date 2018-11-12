3419ab43c18619f0cf574bc2623b62c8cf1a4d61
DESKTOP-VS617K3\YUKI <yukigotuuu@gmail.com>
2018-11-12 12:30:14 +0900

    nav追加

---
+++ app.js
@@ -0,0 +1,8 @@
+
+jQuery(function($) {
+    $('.bg-slider').bgSwitcher({
+        images: ['https://crayonimg.e-shops.jp/cms-dimg/1047531/195503/600_450.jpg',
+        'https://t-ec.bstatic.com/xdata/images/hotel/max500/124351844.jpg?k=b569a6e3cdb959aeb8f9df51ac097229426f6f58d1968f5a3d77e59888807379&o=',
+        'https://media-cdn.tripadvisor.com/media/daodao/photo-s/0f/c9/df/4b/photo4jpg.jpg'],
+    });
+});
